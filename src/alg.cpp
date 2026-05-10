8e0fecb286e7bfe60500c6e1e49a724769301657
anton <ashtanyuk@gmail.com>
2026-05-10 14:21:50 +0300

    Начальный коммит

---
+++ src/alg.cpp
@@ -0,0 +1,10 @@
+// Copyright 2021 NNTU-CS
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "bst.h"
+
+void makeTree(BST<std::string>& tree, const char* filename) {
+  // поместите сюда свой код
+}
